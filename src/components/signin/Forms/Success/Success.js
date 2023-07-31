1bec3b8acb7ae122f87bdf71c6060040db5fd24d
kao19 <feiyukine@gmail.com>
2023-07-31 09:55:21 +0100

    refactoring to registration with redux is done

--- src/components/signin/Forms/Success/Success.js
+++ src/components/signin/Forms/Success/Success.js
@@ -1,55 +1,40 @@
-import React, { useContext, useEffect } from "react";
-import { FormContext } from "../../../../containers/FormContainer";
-import config from '../../../../config';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
-
-const apiURI = config.apiURI;
+import { setFormData } from "../../../../actions/registrationActions";
+import { submitFormData } from "../../../../actions/registrationActions";
 
 function Success() {
-  const { formData } = useContext(FormContext);
+  const dispatch = useDispatch();
+  const formData = useSelector((state) => state.registration.formData);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const formDataWithoutConfirmPassword = { ...formData };
   delete formDataWithoutConfirmPassword.confirmPassword;
-  
-  const client = formDataWithoutConfirmPassword.type;
 
   useEffect(() => {
-    const callAPI = async () => {
-      try {
-        const apiUrl = apiURI + "/api/v1/register/client/" + client;
-        const requestOptions = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formDataWithoutConfirmPassword),
-        };
-    
-        const response = await fetch(apiUrl, requestOptions);
-    
-        if (response.ok) {
-          const data = await response.json();
-          console.log("API Response:", data);
-        } else {
-          console.error("API Error:", response.statusText);
-        }
-      } catch (error) {
-        console.error("API Error:", error);
-      }
-    };
-    
-    callAPI();
-  }, [client, formDataWithoutConfirmPassword]);
+    dispatch(setFormData(formDataWithoutConfirmPassword));
+    dispatch(submitFormData(formDataWithoutConfirmPassword));
+  }, [formDataWithoutConfirmPassword, dispatch]);
+
+  const apiStatus = useSelector((state) => state.registration.apiStatus);
 
   return (
     <div className="font-medium">
-      Informations bien reçu. Vérifier votre email pour poursuivre votre inscription puis vous pouvez s'inscrire.
-      <div className="mt-4">
-        <Link to="/" className="text-blue-500 underline">
-          s'inscrire
-        </Link>
-      </div>
+      {apiStatus === "success" ? (
+        <div>
+          Informations bien reçu. Vérifier votre email pour poursuivre votre inscription puis vous pouvez vous inscrire.
+          <div className="mt-4">
+            <Link to="/" className="text-blue-500 underline">
+              s'inscrire
+            </Link>
+          </div>
+        </div>
+      ) : apiStatus === "loading" ? (
+        <div>Loading...</div>
+      ) : (
+        <div>Error occurred while processing the request.</div>
+      )}
     </div>
   );
 }
