2a464c86ba5cb5e815bfee0bef4c71413b5fdc77
kao19 <feiyukine@gmail.com>
2023-08-01 12:14:26 +0100

    update client Notifications.js

--- src/dashboards/client/Notifications.js
+++ src/dashboards/client/Notifications.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchNotifications } from '../../actions/notificationActions';
-import { FaBell } from 'react-icons/fa'; // Import the FaBell icon
+import { FaBell } from 'react-icons/fa'; 
+
+// Function to format date as "01 Aug"
+function getFormattedDate(dateString) {
+  const date = new Date(dateString);
+  const options = { day: '2-digit', month: 'short' };
+  return new Intl.DateTimeFormat('fr-FR', options).format(date);
+}
 
 function Notifications({ userId, notifications, loading, error, fetchNotifications }) {
   useEffect(() => {
     fetchNotifications(userId);
   }, [fetchNotifications, userId]);
+
+  // Extract unique dates from notifications
+  const uniqueDates = Array.from(new Set(notifications.map(notification => getFormattedDate(notification.dateEnvoie))));
 
   return (
     <div>
@@ -16,20 +26,43 @@ function Notifications({ userId, notifications, loading, error, fetchNotificatio
       ) : error ? (
         <p>Error: {error}</p>
       ) : notifications && notifications.length ? (
-        <ul>
-          {notifications.map((notification) => (
-            <li key={notification.id} className="bg-white p-4 rounded-lg border border-gray-300 shadow mb-4 flex items-center">
-              <div className="inline-block mr-4">
-                    <FaBell className="w-6 h-6 text-orange-500 mr-4" /> {/* Notification icon */}
+        <>
+          {uniqueDates.map(date => (
+            <div key={date} className="mb-4">
+              <div className="flex justify-center"> 
+                <div className="w-20 h-12 bg-gray-200 rounded-lg flex items-center justify-center text-sm mb-2">
+                  {date}
+                </div>
               </div>
-              <div>
-                <h3 className="text-lg font-semibold mb-2">{notification.titre}</h3>
-                <p className="text-sm mb-4">{notification.contenu}</p>
-                <p className="text-xs text-gray-400">{new Date(notification.dateEnvoie).toLocaleString()}</p>
-              </div>
-            </li>
+              <ul>
+                {notifications.map((notification) => {
+                  if (getFormattedDate(notification.dateEnvoie) === date) {
+                    return (
+                      <li
+                        key={notification.id}
+                        className="bg-white p-4 rounded-lg border border-gray-300 shadow mb-4 flex items-start"
+                      >
+                        <div className="flex items-center"> 
+                          <div className="w-6 h-6 text-orange-500 mr-4">
+                            <FaBell /> 
+                          </div>
+                          <div>
+                            <h3 className="text-lg font-semibold mb-2">{notification.titre}</h3>
+                            <p className="text-sm mb-4">{notification.contenu}</p>
+                            <p className="text-xs text-gray-400">
+                              {new Date(notification.dateEnvoie).toLocaleString()}
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  }
+                  return null;
+                })}
+              </ul>
+            </div>
           ))}
-        </ul>
+        </>
       ) : (
         <p>No notifications available.</p>
       )}
