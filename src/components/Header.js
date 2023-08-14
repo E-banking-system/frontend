ecc53553f6a4ee2e95650ee3ef02ccb07d28d4c6
kao19 <feiyukine@gmail.com>
2023-08-14 15:32:34 +0100

    isolate the header in a component to reuse it

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FiMessageSquare, FiBell } from 'react-icons/fi';
+
+function Header() {
+  return (
+    <div className="flex items-center">
+      <div className="relative ml-2">
+        <FiMessageSquare className="h-6 w-6 text-gray-500" />
+        <span className="absolute top-0 right-0 rounded-full bg-red-500 text-white text-xs leading-tight px-1">
+          3
+        </span>
+      </div>
+      <div className="relative ml-2 mr-2">
+        <FiBell className="h-6 w-6 text-gray-500" />
+        <span className="absolute top-0 right-0 rounded-full bg-red-500 text-white text-xs leading-tight px-1">
+          5
+        </span>
+      </div>
+      <span className="text-lg font-semibold mr-8">Welcome, John Doe</span>
+    </div>
+  );
+}
+
+export default Header;
--- src/dashboards/client/AccountList.js
+++ src/dashboards/client/AccountList.js
@@ -6,6 +6,7 @@ import ViewAccount from './ViewAccount';
 import VirementUnitaireForm from './VirementUnitaireForm';
 import VirementPermanantForm from './VirementPermanantForm';
 import AccountOperations from './AccountOperations';
+import Header from '../../components/Header';
 
 function AccountsList({ data, loading, error, fetchAccountsClient }) {
   const [searchTerm, setSearchTerm] = useState('');
@@ -69,21 +70,7 @@ function AccountsList({ data, loading, error, fetchAccountsClient }) {
             </div>
         </div>
         
-        <div className="flex items-center ">
-          <div className="relative ml-2">
-            <FiMessageSquare className="h-6 w-6 text-gray-500" />
-            <span className="absolute top-0 right-0 rounded-full bg-red-500 text-white text-xs leading-tight px-1">
-              3
-            </span>
-          </div>
-          <div className="relative ml-2 mr-2">
-            <FiBell className="h-6 w-6 text-gray-500" />
-            <span className="absolute top-0 right-0 rounded-full bg-red-500 text-white text-xs leading-tight px-1">
-              5
-            </span>
-          </div>
-          <span className="text-lg font-semibold mr-8">Welcome, John Doe</span>
-        </div>
+        <Header />
         
       </nav>
 
