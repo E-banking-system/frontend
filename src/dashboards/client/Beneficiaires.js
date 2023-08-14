78dcbb950658d93ed03957eed5e17ea2a19d8ab6
kao19 <feiyukine@gmail.com>
2023-08-14 12:08:49 +0100

    remove delete beneficier

--- src/dashboards/client/Beneficiaires.js
+++ src/dashboards/client/Beneficiaires.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import {  fetchBeneficiaires, addBeneficiaire, deleteBeneficiaire } from '../../actions/beneficierActions';
 import BeneficiaireForm from './BeneficiaireForm';
-import { FiEye, FiTrash2 } from 'react-icons/fi';
+import { FiEye } from 'react-icons/fi';
 import CustomAlert from '../../components/CustomAlert';
 import UpdateBeneficiaire from './UpdateBeneficiaire';
 
@@ -12,7 +12,6 @@ const Beneficiaire = ({
   error,
   fetchBeneficiaires,
   addBeneficiaire,
-  deleteBeneficiaire,
 }) => {
   const [showForm, setShowForm] = useState(false);
   const [formError, setFormError] = useState(null);
@@ -36,17 +35,7 @@ const Beneficiaire = ({
         setFormError(error.message || 'An error occurred');
       });
   };
-
-  const handleDeleteBeneficiaire = async (beneficiaire) => {
-    try {
-      await deleteBeneficiaire(beneficiaire.beneficier_id);
-    } catch (error) {
-     setAlertMessage("You are not allowed to delete this beneficiary.");
-    }
-  };
   
-  
-
   const handleButtonClick = () => {
     setShowForm(true);
   };
@@ -83,8 +72,8 @@ const Beneficiaire = ({
               <th className="px-4 py-2 bg-gray-100 border border-gray-300">Prénom</th>
               <th className="px-4 py-2 bg-gray-100 border border-gray-300">Numéro de Compte</th>
               <th className="px-4 py-2 bg-gray-100 border border-gray-300">email</th>
+              <th className="px-4 py-2 bg-gray-100 border border-gray-300">RIB</th>
               <th className="px-2 py-1 bg-gray-100 border border-gray-300">modifier</th>
-              <th className="px-2 py-1 bg-gray-100 border border-gray-300">supprimer</th>
             </tr>
           </thead>
           <tbody>
@@ -105,17 +94,7 @@ const Beneficiaire = ({
                     </div>
                   </button>
                 </td>
-                <td className="px-2 py-1 border border-gray-300" style={{ textAlign: 'center' }}>
-                  <button
-                    onClick={() => handleDeleteBeneficiaire(beneficiaire)}
-                    style={{ margin: '0 auto' }}
-                    className="focus:outline-none"
-                  >
-                    <div>
-                      <FiTrash2 />
-                    </div>
-                  </button>
-                </td>
+                
               </tr>
             ))}
           </tbody>
