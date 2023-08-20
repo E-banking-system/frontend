4b3969f64fd0c2da46f88b34ee84f8689f479315
kao19 <feiyukine@gmail.com>
2023-08-20 15:17:39 +0100

    handling on click in backoffice navbar items

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,22 +12,37 @@ function Header({fetchNbrNotif,nbrNotif, fetchNbrMsg, nbrMsg}) {
     fetchNbrMsg();
   }, []);
 
+  const handleClickMessages = () => {
+    localStorage.setItem('activeButton', 'chat')
+    window.location.reload();
+  }
+
+  const handleClickNotif = () => {
+    localStorage.setItem('activeButton', 'notifications')
+    window.location.reload();
+  }
+
+  const handleClickProfile = () => {
+    localStorage.setItem('activeButton', 'profile')
+    window.location.reload();
+  }
+
   
   return (
     <div className="flex items-center">
       <div className="relative ml-2 mr-8">
-        <FiMessageSquare className="h-6 w-6 text-gray-500 mt-3" />
+        <FiMessageSquare className="h-6 w-6 text-gray-500 mt-3" onClick={handleClickMessages} />
         <span className="absolute top-2 left-3 rounded-full bg-red-500 text-white text-xs leading-tight px-1">
           {nbrMsg}
         </span>
       </div>
       <div className="relative ml-2 mr-8">
-        <FiBell className="h-6 w-6 text-gray-500 mt-3" />
+        <FiBell className="h-6 w-6 text-gray-500 mt-3" onClick={handleClickNotif}/>
         <span className="absolute top-2 left-3 rounded-full bg-red-500 text-white text-xs leading-tight px-1">
           {nbrNotif}
         </span>
       </div>
-      <div>
+      <div onClick={handleClickProfile}>
         <span className="text-lg font-semibold">{localStorage.getItem("nom")} {localStorage.getItem("prenom")}</span>
         <br />
         <span className="text-sm text-gray-500 flex flex-col items-center justify-center">{localStorage.getItem("role")}</span>
